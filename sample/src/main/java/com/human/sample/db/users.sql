25ad0f05156d5b6c2cc650f54c62971384d6c387
lione78 <136799160+lione78@users.noreply.github.com>
2023-11-20 17:00:08 +0900

    SQL

--- sample/src/main/java/com/human/sample/db/users.sql
+++ sample/src/main/java/com/human/sample/db/users.sql
@@ -6,3 +6,11 @@ create table users(
     regDate date default (CURRENT_DATE),
     isDeleted number(10,0) default 0
 )
+
+select * from users;
+
+update users set isDeleted=0 where "uid" = 'maria';
+
+commit;
+
+update users set pwd='', uname='', email='' where "uid"='';
